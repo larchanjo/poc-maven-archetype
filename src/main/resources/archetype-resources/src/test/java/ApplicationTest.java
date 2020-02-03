0206f64310ac7336175c2ffca316790d97f699c6
Luram Archanjo <luram.archanjo@sensedia.com>
2020-02-03 13:47:16 -0300

    Initial Project

---
+++ src/main/resources/archetype-resources/src/test/java/ApplicationTest.java
@@ -0,0 +1,14 @@
+package $package;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+
+}
